545f5f21c5c2de0c5fbc38151d82725a7c89c138
Jagni Dasa <jagnidasa@gmail.com>
2020-10-19 15:03:20 -0300

    Portfolio hexgrid; HexgridChild: ProjectCircle

--- lib/screens/portfolio/index.dart
+++ lib/screens/portfolio/index.dart
@@ -1,11 +1,19 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
+import 'package:hexagonal_grid_widget/hex_grid_context.dart';
+import 'package:hexagonal_grid_widget/hex_grid_widget.dart';
 import 'package:iamjagni/models/portfolio/project/index.dart';
-import 'package:iamjagni/screens/portfolio/widgets/card.dart';
-import 'package:iamjagni/utils/layout.dart';
+import 'package:iamjagni/screens/portfolio/widgets/circle.dart';
 import 'package:iamjagni/widgets/store_stream_builder.dart';
 
 class PortfolioScreen extends StatelessWidget {
+  final double _minHexWidgetSize = 0;
+  final double _scaleFactor = 0.5;
+  final double _densityFactor = 2;
+  final double _velocityFactor = 0.3;
+  final bool _flatLayout = true;
+
   @override
   Widget build(BuildContext context) {
     return StoreStreamBuilder<List<Project>>(builder: (projects, context) {
@@ -16,33 +24,53 @@ class PortfolioScreen extends StatelessWidget {
   }
 
   buildList(List<Project> projects, BuildContext context) {
-    return Center(
-      child: Container(
-        width: AppLayout.maxContentWidth(context),
-        child: GridView.builder(
-            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                maxCrossAxisExtent: AppLayout.maxContentWidth(context) /
-                    AppLayout.numberOfContentColumns(context),
-                childAspectRatio: 1,
-                crossAxisSpacing: AppLayout.paddingSize / 2,
-                mainAxisSpacing: AppLayout.paddingSize / 2),
-            itemCount: projects.length,
-            physics: ClampingScrollPhysics(),
-            shrinkWrap: true,
-            padding: EdgeInsets.symmetric(vertical: AppLayout.paddingSize),
-            itemBuilder: (BuildContext context, int index) {
-              return AnimationConfiguration.staggeredGrid(
-                  position: index,
-                  duration: const Duration(milliseconds: 800),
-                  columnCount: 2,
-                  child: SlideAnimation(
-                      verticalOffset: 100,
-                      child: Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: FadeInAnimation(
-                            child: PortfolioCard(project: projects[index])),
-                      )));
-            }),
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: LayoutBuilder(
+              builder: (context, constraints) {
+                final containerSize =
+                    min(constraints.maxWidth, constraints.maxHeight);
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Center(
+                    child: Container(
+                      width: containerSize,
+                      height: containerSize,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                            color: Colors.green.withAlpha(100), width: 5),
+                        borderRadius: BorderRadius.all(Radius.circular(
+                          containerSize / 2,
+                        )),
+                      ),
+                      child: ClipOval(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(
+                                Radius.circular(containerSize / 2)),
+                          ),
+                          child: HexGridWidget(
+                              children: projects
+                                  .map((project) => ProjectCircle(project))
+                                  .toList(),
+                              hexGridContext: HexGridContext(
+                                  _minHexWidgetSize,
+                                  containerSize / 3,
+                                  _scaleFactor,
+                                  _densityFactor,
+                                  _velocityFactor,
+                                  _flatLayout)),
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/screens/portfolio/widgets/circle.dart
@@ -0,0 +1,75 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:hexagonal_grid/hexagonal_grid.dart';
+import 'package:hexagonal_grid_widget/hex_grid_child.dart';
+import 'package:hexagonal_grid_widget/hex_grid_context.dart';
+import 'package:iamjagni/models/portfolio/project/index.dart';
+import 'package:iamjagni/screens/portfolio/details.dart';
+import 'package:iamjagni/widgets/image/circle_image.dart';
+
+class ProjectCircle extends HexGridChild {
+  final Project project;
+  ProjectCircle(this.project);
+
+  @override
+  Widget toHexWidget(BuildContext context, HexGridContext hexGridContext,
+      double size, UIHex hex) {
+    return Container(
+      padding: EdgeInsets.all((hexGridContext.maxSize - size) / 2),
+      child: Container(
+          width: size,
+          height: size,
+          child: Card(
+              margin: EdgeInsets.all(0),
+              clipBehavior: Clip.antiAlias,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(size)),
+              child: InkWell(
+                  onTap: () {
+                    Navigator.push(context,
+                        CupertinoPageRoute(builder: (context) {
+                      return PortfolioDetails(project: project);
+                    }));
+                  },
+                  child: Center(
+                    child: Padding(
+                        padding: EdgeInsets.all(4), child: buildImage(project)),
+                  )))),
+    );
+  }
+
+  @override
+  double getScaledSize(
+      HexGridContext hexGridContext, double distanceFromOrigin) {
+    double scaledSize = hexGridContext.maxSize -
+        (distanceFromOrigin * hexGridContext.scaleFactor);
+    return max(scaledSize, hexGridContext.minSize);
+  }
+
+  buildImage(Project project) {
+    var url = project.data.iconUrl;
+    final asset = url == null;
+    if (asset) {
+      url = "assets/images/avatar.png";
+    }
+    return Hero(
+        transitionOnUserGestures: true,
+        tag: project.uid + "icon",
+        child: CircleImage(url: url, asset: asset));
+  }
+
+  buildTitle(Project project, BuildContext context) {
+    final child = Text(
+      project.data.title,
+      style: Theme.of(context).textTheme.headline5,
+      textAlign: TextAlign.center,
+      maxLines: 1,
+    );
+    return Hero(
+        transitionOnUserGestures: true,
+        tag: project.uid + "title",
+        child: child);
+  }
+}
